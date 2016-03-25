cc817ea20a8492ccd5251b3f5b759a9728e9d5f9
Cyrus & Sharon <pair+cyvattes.sharonjean@devbootcamp.com>
2016-03-24 18:32:38 -0700

    Complete migrations and validations

---
+++ app/models/lit.rb
@@ -0,0 +1,3 @@
+class Lit < ActiveRecord::Base
+  belongs_to :user
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,14 @@
-10.times {User.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, user_name: Faker::Internet.user_name, email: Faker::Internet.email, password: 'test1234')}
+10.times {
+  User.create(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    user_name: Faker::Internet.user_name,
+    email: Faker::Internet.email,
+    password: Faker::Internet.password
+    )}
+
+25.times {
+  Lit.create(
+    user_id: rand(1..10),
+    lit_text: Faker::Hipster.sentence
+    )}
