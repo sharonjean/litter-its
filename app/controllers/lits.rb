cf4d68af7f14c3f075e6a74638b0d8bb9c097af3
Sharon <email": null>
2016-03-25 11:54:52 -0700

    Work on Login routes

--- app/controllers/lits.rb
+++ app/controllers/lits.rb
@@ -1,18 +1,29 @@
-get '/lits/new' do
-  @lits
-  erb :'/lits/new'
-end
-
 post '/lits' do
-  @lit = Lit.create(lit_text: params[:lit_text])
-
+  if @session[:id] == nil
+    @user = User.find(params[])
+    @lit = Lit.create(lit_text: params[:lit_text])
+    redirect '/lits'
+  else
+    @lit.errors.full_messages
+    redirect '/login'
+  end
 end
+
+get '/lits/new' do
+  @users = User.all
+  @lits = Lit.all
+  erb :'/lits/index'
+end
+
 
 delete '/lits' do
+  @lit.find_by(id: params[:id])
+  @lit.destroy
+  redirect '/lits'
 end
 
 
-s
+
 
 
 
